6fef5f47a77ea17ade213ff6574da6ad59a5a375
IOStao <baidu@wztdemacbook-pro.local>
2015-10-19 15:09:29 +0800

    Initial Commit

---
+++ 韬微博4.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  韬微博4.0
+//
+//  Created by wzt on 15/10/19.
+//  Copyright © 2015年 Baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 韬微博4.0/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  韬微博4.0
+//
+//  Created by wzt on 15/10/19.
+//  Copyright © 2015年 Baidu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
